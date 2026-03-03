bd5bbf7d7edb7c7fc03790cfbe341197aadc06fa
dlgarciazav <dl.garcia.zavala@ugto.mx>
2026-03-03 15:27:31 -0600

    Actualización

--- conditions/sufficient_decrease.py
+++ conditions/sufficient_decrease.py
@@ -1,17 +1,9 @@
 # Diana
 import numpy as np
 
-def check_sufficient_decrease(f, x, p, alpha, grad, c1=1e-4):
-    """
-    Verifica la condición de Armijo (Sufficient Decrease).
-    f: función objetivo
-    x: punto actual
-    p: dirección de descenso (GD o Newton)
-    alpha: longitud de paso a evaluar
-    grad: gradiente en el punto actual x
-    """
+def check_sufficient_decrease(function, x, p, alpha, grad, c1=1e-4):
+
     # f(x + a*p) <= f(x) + c1 * a * grad^T * p
-    lhs = f(x + alpha * p)
-    rhs = f(x) + c1 * alpha * np.dot(grad, p)
-    
-    return lhs <= rhs 
+    f_new = function.Eval(x + alpha * p)
+    f_old = function.Eval(x)
+    return f_new <= f_old + c1 * alpha * np.dot(grad, p)
